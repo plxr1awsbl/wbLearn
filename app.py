07ff8fa853b6ad5067c33e92d4b113dd4a1b5638
Администратор <bodyasbb@gmail.com>
2022-10-30 03:37:15 +0300

    123

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
-from collections import namedtuple
-
 from flask import Flask, render_template, redirect, url_for, request
 
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
-app.config['SQLALCHEMY DATABASE_URL'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/diiiizDB.db'
 db = SQLAlchemy(app)
 
-Message = namedtuple('Message', 'text tag')
-messages = []
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(1024), nullable=False)
+
+    def __init__(self, text, tags):
+        self.tags = [
+            Tag(tag.strip() for tag in tags.split(','))
+        ]
+        self.text = text.strip()
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(32), nullable=False)
+
+    message_id = db.Column(db.Integer, db.ForeignKey('message.id'), nullable=False)
+    message = db.relationship('Message')
+
+
+db.create_all()
 
 
 @app.route('/', methods=['GET'])
@@ -19,7 +37,7 @@ def hello_world():
 
 @app.route('/main', methods=['GET'])
 def main():
-    return render_template('main.html', messages=messages)
+    return render_template('main.html', messages=Message.query.all())
 
 
 @app.route('/add_message', methods=['POST'])
@@ -27,6 +45,7 @@ def add_message():
     text = request.form['text']
     tag = request.form['tag']
 
-    messages.append(Message(text, tag))
+    db.session.add(Message(text, tag))
+    db.session.commit()
 
     return redirect(url_for('main'))
